e9a75dd015f0cc7b927d81f93dbd7b3ef42cfbbb
Samuel Xavier <samuelxpp@gmail.com>
2024-03-13 13:10:48 -0300

    Database creation logic updated

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pandas
 
 con = duckdb.connect("database.db")
 
-# con.sql("CREATE SEQUENCE id_sequence START 1")
+con.sql("CREATE SEQUENCE id_sequence START 1")
+
 con.sql("DROP TABLE lor_scraping_t")
 
 con.sql("CREATE TABLE lor_scraping_t (name VARCHAR, rank INT64, lp DOUBLE, date VARCHAR, id INT DEFAULT nextval('id_sequence') PRIMARY KEY)")
