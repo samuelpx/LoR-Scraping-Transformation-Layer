d8eeec128876037f1766e2cdfb58be9fb26eb387
Samuel Xavier <samuelxpp@gmail.com>
2024-03-13 17:48:11 -0300

    Updating table format

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import duckdb
 
+#Initiate the db if it does not exist, create a cursor otherwise.
 con = duckdb.connect("database.db")
 
+#Initiating a sequence in order for the primary key of our table to auto-increment
 con.sql("CREATE SEQUENCE id_sequence START 1")
 
+#Since we are creating the whole table from a monolithic parquet file
+#we need to make sure the table is drop to avoid redundancy
 con.sql("DROP TABLE lor_scraping_t")
 
-con.sql("CREATE TABLE lor_scraping_t (name VARCHAR, rank INT64, lp DOUBLE, date VARCHAR, id INT DEFAULT nextval('id_sequence') PRIMARY KEY)")
+#Creating our table, setting data-types 
+#adding the auto-incrementable primary key
+con.sql("CREATE TABLE lor_scraping_t (id INT DEFAULT nextval('id_sequence') PRIMARY KEY, name VARCHAR, rank INT64, lp DOUBLE, date VARCHAR)")
 
+#Inserting data from a direct query to the parquet file
 con.sql("INSERT INTO lor_scraping_t (name, rank, lp, date) select name, rank, lp, date FROM read_parquet('transformed_data.parquet')")
