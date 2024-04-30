5b85d3e6f8eff395c8fa4c6d4ef08d9b94919341
Samuel Xavier <samuelxpp@gmail.com>
2024-04-30 15:37:16 -0300

    Automating data ingestion

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ import duckdb
 con = duckdb.connect("database.db")
 
 #Initiating a sequence in order for the primary key of our table to auto-increment
-con.sql("CREATE SEQUENCE id_sequence START 1")
+try:
+    con.sql("CREATE SEQUENCE id_sequence START 1")
+except duckdb.duckdb.CatalogException:
+    print("Sequence for Primary key already exists")
 
 #Since we are creating the whole table from a monolithic parquet file
 #we need to make sure the table is drop to avoid redundancy
-con.sql("DROP TABLE lor_scraping_t")
+try:
+    con.sql("DROP TABLE lor_scraping_t")
+except duckdb.duckdb.CatalogException:
+    print("Table did not exist, initializing table")
 
 #Creating our table, setting data-types 
 #adding the auto-incrementable primary key
 con.sql("CREATE TABLE lor_scraping_t (id INT DEFAULT nextval('id_sequence') PRIMARY KEY, name VARCHAR, rank INT64, lp DOUBLE, date VARCHAR)")
 
 #Inserting data from a direct query to the parquet file
-con.sql("INSERT INTO lor_scraping_t (name, rank, lp, date) select name, rank, lp, date FROM read_parquet('transformed_data.parquet')")
+con.sql("INSERT INTO lor_scraping_t (name, rank, lp, date) select name, rank, lp, date FROM read_parquet('/home/samuelpx/Documents/Projects/python/LoR-Scraping/transformed_data.parquet')")
